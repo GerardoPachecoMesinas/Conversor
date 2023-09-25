8c9f3475975acc4653d602b837c9877563e2cd9e
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2023-09-25 14:55:02 -0600

    Add first content

---
+++ conversor.js
@@ -0,0 +1,28 @@
+function cmToIn(cm) {
+    let resultInch;
+    resultInch = cm / 2.54;
+    pResult.innerHTML = `${cm} centimetros equivalen a ${resultInch} pulgadas`;
+}
+
+function inToCm(inch) {
+    let resultCm;
+    resultCm = inch * 2.54;
+    pResult.innerHTML = `${inch} pulgadas equivalen a ${resultCm} centimetros`;
+}
+
+const cm = Number(document.querySelector('#cm'));
+const inch = Number(document.querySelector('#in'));
+const buttom = document.querySelector('#buttom');
+const pResult = document.querySelector('#pResult');
+
+console.log({
+    cm,
+    inch,
+    buttom,
+    result
+    }
+)
+addEventListener.
+addEventListener.
+addEventListener.
+addEventListener.
