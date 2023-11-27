391cc4ae3b6a5266ea38125232445ed8cece528d
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2023-11-27 11:30:16 -0600

    with jquery and changed inputs

--- conversor.js
+++ conversor.js
@@ -1,34 +1,54 @@
 function cmInput() {
-    var cm = document.querySelector('#input1');
+    // var cm = document.querySelector('#input1');
+    var cm = $('#input1').val();
     return cm;
 }
 function inchInput() {
-    var inch = document.querySelector('#input2');
+    // var inch = document.querySelector('#input2');
+    var inch = $('#input2').val();
     return inch;
 }
 
 function cmToIn() {
-    var cm = Number(cmInput().value);
+    var cm = Number(cmInput());
     var resultInch = cm / 2.54;
     return resultInch
 }
 function inToCm() {
-    var inch = Number(inchInput().value);
+    var inch = Number(inchInput());
     var resultCm = inch * 2.54;
     return resultCm
 }
 
-function print() {
-    const pResult = document.querySelector('#pResult');
+$("#input1").on("input", function () {
+    $("#input2").val(cmToIn());
+});
 
-    if (cmInput().value) {
-        pResult.innerHTML = `${cmInput().value} centimetros equivalen a ${cmToIn()} pulgadas`;
-    } else if (inchInput().value) {
-        pResult.innerHTML = `${inchInput().value} pulgadas equivalen a ${inToCm()} centimetros`;
-    } else {
-        alert("ingresa un valor")
-    }
-}
+$("#input2").on("input", function () {
+    $("#input1").val(inToCm());
+});
 
-const buttom = document.querySelector('#buttom_calcular');
-buttom.addEventListener('click', print)
+$("#input1").on("click", function () {
+    $("#input1").val("");
+    $("#input2").val("");
+});
+
+$("#input2").on("click", function () {
+    $("#input2").val("");
+    $("#input1").val("");
+});
+
+// function print2() {
+//     const pResult = document.querySelector('#pResult');
+
+//     if (cmInput().value) {
+//         pResult.innerHTML = `${cmInput().value} centimetros equivalen a ${cmToIn()} pulgadas`;
+//     } else if (inchInput().value) {
+//         pResult.innerHTML = `${inchInput().value} pulgadas equivalen a ${inToCm()} centimetros`;
+//     } else {
+//         alert("ingresa un valor")
+//     }
+// }
+
+// const buttom = document.querySelector('#buttom_calcular');
+// buttom.addEventListener('click', print)
